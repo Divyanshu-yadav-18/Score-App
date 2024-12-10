164d55dd807dd3ecef212277e92a4f113c264349
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2024-12-10 13:44:45 +0530

    first commit

---
+++ lib/Pages/score_app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ScoreApp extends StatefulWidget {
+  const ScoreApp({super.key});
+
+  @override
+  State<ScoreApp> createState() => _ScoreAppState();
+}
+
+class _ScoreAppState extends State<ScoreApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title:const Text("SCORING APP", style: TextStyle(color: Colors.black),),
+        centerTitle: true,
+      ),
+      
+      body: ,
+    );
+  }
+}
