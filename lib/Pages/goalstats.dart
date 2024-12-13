b12a0071a512c8af17b76fe20cd4852f6d18d08a
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2024-12-13 16:57:57 +0530

    handling null condition

--- lib/Pages/goalstats.dart
+++ lib/Pages/goalstats.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 
 Widget goalStats(int expandedTime, int homegoal, int awaygoal) {
+  var home = homegoal;
+  var away = awaygoal;
+  var elapsed = expandedTime;
+
+  if (home == null) home = 0;
+  if (away == null) away = 0;
+  if (elapsed == null) elapsed = 0;
   return Expanded(
     child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Text(
-          '$expandedTime',
+          '$elapsed',
           style: const TextStyle(fontSize: 30.0),
         ),
         Expanded(
             child: Center(
           child: Text(
-            '$homegoal-$awaygoal',
+            '$home-$away',
             textAlign: TextAlign.center,
             style: const TextStyle(fontSize: 36.0),
           ),
--- lib/Pages/match_title.dart
+++ lib/Pages/match_title.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:score_app/model.dart';
 
 Widget matchTitle(ScoreMatch match) {
+  var homegoal = match.goal.home;
+  var awaygoal = match.goal.away;
+  if (homegoal == null) homegoal = 0;
+  if (awaygoal == null) awaygoal = 0;
   return Container(
     margin: const EdgeInsets.symmetric(vertical: 12.0),
     child: Row(
@@ -21,7 +25,7 @@ Widget matchTitle(ScoreMatch match) {
         ),
         Expanded(
           child: Text(
-            '${match.goal.home}-${match.goal.away}',
+            '$homegoal-$awaygoal',
             textAlign: TextAlign.center,
             style: const TextStyle(color: Colors.white, fontSize: 16.0),
           ),
